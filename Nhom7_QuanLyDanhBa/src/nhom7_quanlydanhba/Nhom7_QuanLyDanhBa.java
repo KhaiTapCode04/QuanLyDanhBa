814d5caf5a7f6a9a4717cf7c6de1abde98e7a798
KhaiTapCode04 <167021322+KhaiTapCode04@users.noreply.github.com>
2024-11-18 22:25:09 +0700

    Add files via upload

---
+++ Nhom7_QuanLyDanhBa/src/nhom7_quanlydanhba/Nhom7_QuanLyDanhBa.java
@@ -0,0 +1,19 @@
+package nhom7_quanlydanhba;
+
+import javax.swing.*;
+
+public class Nhom7_QuanLyDanhBa {
+    public static void main(String[] args) {
+        // Tạo cửa sổ JFrame
+        JFrame frame = new JFrame("Quản Lý Danh Bạ");
+        
+        // Đặt kích thước cho form: chiều rộng 800px, chiều cao 480px
+        frame.setSize(480, 800);
+        
+        // Đặt hành động khi đóng cửa sổ
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Hiển thị cửa sổ
+        frame.setVisible(true);
+    }
+}
